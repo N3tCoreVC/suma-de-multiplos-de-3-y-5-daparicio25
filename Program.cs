00818c5a1d2098869e20f2cb0e1d8abd5de98e80
daparicio25 <apariciodaniel250@gmail.com>
2018-10-05 16:54:55 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace suma_de_multiplos_de_3_y_5_daparicio25
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            Console.Write("Ingresa un numero: ");
+
+            if (Int32.TryParse(Console.ReadLine(), out num)) {
+                int suma = 0;
+
+                for (int i=1; i<=num; i++) {
+                    if (i%3 == 0 || i%5 == 0) {
+                        Console.WriteLine(i);
+                        suma += i;
+                    }
+                }
+
+                Console.WriteLine("La suma es: " + suma);
+            } else {
+                Console.WriteLine("El valor introducido no es numérico");
+            }
         }
     }
 }
