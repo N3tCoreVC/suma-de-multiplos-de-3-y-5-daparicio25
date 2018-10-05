1fd8f10defe994026c175f88bb20286e86220454
daparicio25 <apariciodaniel250@gmail.com>
2018-10-05 16:49:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace suma_de_multiplos_de_3_y_5_daparicio25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
